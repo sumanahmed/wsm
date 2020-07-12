dd020c5349d171e17176856b4679bbbad6b6728f
sumanahmed <suman777333@gmail.com>
2020-07-12 17:02:37 +0600

    mehtod added to get all clients

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
 use Illuminate\Http\Request;
 
 class ClientController extends Controller
 {
-    //
+    //get all clients
+    public function index(){
+        $clients = Client::all();
+        return $clients;
+    }
+
+
+
 }
