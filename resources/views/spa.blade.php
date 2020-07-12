91c32fac169a795d15b6faa601a54f21d0dddb42
sumanahmed <suman777333@gmail.com>
2020-07-12 15:04:44 +0600

    title removed from head tag

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -3,7 +3,6 @@
 	<head>
 	  <meta charset="utf-8">
 	  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-	  <title>WSM | Dashboard</title>
 	  <!-- Tell the browser to be responsive to screen width -->
 	  <meta name="viewport" content="width=device-width, initial-scale=1">
 	  <!-- Font Awesome -->
