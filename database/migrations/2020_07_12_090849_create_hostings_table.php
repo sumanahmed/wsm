ea07afb053270f4923f9803fc685369402706559
sumanahmed <suman777333@gmail.com>
2020-07-12 15:28:03 +0600

    hosting migration to manages hosting

---
+++ database/migrations/2020_07_12_090849_create_hostings_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHostingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hostings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('client_id');
+            $table->string('domain_link');
+            $table->date('purchase_date');
+            $table->date('renew_date');
+            $table->decimal('price', 10, 2);
+            $table->integer('disk_size');
+            $table->text('note')->nullable();
+            $table->foreign('client_id')->references('clients')->on('id')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hostings');
+    }
+}
