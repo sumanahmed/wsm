1ea5d91a5153e47dd39e5ef6d454d4cd91416645
sumanahmed <suman777333@gmail.com>
2020-07-14 12:29:27 +0600

    method added to client store

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Client;
 use Illuminate\Http\Request;
+use Validator;
+use Response;
 
 class ClientController extends Controller
 {
@@ -13,6 +15,34 @@ class ClientController extends Controller
         return $clients;
     }
 
-
+    //client store
+    public function store(Request $request){
+        $validators = Validator::make($request->all(),[
+            'name'  => 'required',
+            'email'  => 'required',
+            'phone'  => 'required',
+            'address'  => 'required',
+        ]);
+        if($validators->fails()){ 
+            return Response::json(['errors'=>$validators->getMessageBag()->toArray()]);
+        }else{
+            $client             = new Client();
+            $client->name       = $request->name;
+            $client->email      = $request->email;
+            $client->phone      = $request->phone;
+            $client->address    = $request->address;
+            if($client->save()){
+                return response([
+                    'status' => 'success',
+                    'data'   => $client
+                ],201);
+            }else{
+                return response([
+                    'status' => 'error',
+                    'data'    => []
+                ], 403);
+            }
+        }
+    }
 
 }
